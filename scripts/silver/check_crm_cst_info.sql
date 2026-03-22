dd8e71d666bc7c225087c38718b24567817ab4cb
asaad-intelmatix <a.asaad@intelmatix.ai>
2026-03-22 13:26:19 +0200

    Rename data quality check scripts for bronze and silver crm_cst_info tables

--- scripts/silver/check_crm_cst_info.sql
+++ scripts/silver/check_crm_cst_info.sql
@@ -1,9 +1,15 @@
+/**
+ * This script performs data quality checks on the bronze.crm_cst_info table.
+ * It checks for nulls or duplicates in the primary key, unwanted spaces in string columns,
+ * and valid values in categorical columns.
+ */
+ 
 -- Check for nulls or duplicates in primary key
 -- Exception: No records
 
 select 
 COUNT(*)
-from bronze.crm_cst_info
+from silver.crm_cst_info
 group by cst_id 
 having COUNT(*) > 1 or cst_id  is null
 
@@ -11,14 +17,14 @@ having COUNT(*) > 1 or cst_id  is null
 
 -- Check for unwanted spaces in string columns
 select cst_key
-from bronze.crm_cst_info
+from silver.crm_cst_info
 where cst_key != trim(cst_key)
 
 -----
 
 -- Check for valid values in categorical columns
 select distinct cst_marital_status
-from bronze.crm_cst_info cci
+from silver.crm_cst_info cci
 
 select distinct cst_gndr
-from bronze.crm_cst_info cci
+from silver.crm_cst_info cci
