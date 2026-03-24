a609bfac42016661a8976b69dea6418e7d1d60f3
asaad-intelmatix <a.asaad@intelmatix.ai>
2026-03-24 14:59:49 +0200

    Update data types for sales details and add missing product info table in silver layer

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -24,12 +24,24 @@ CREATE TABLE IF NOT EXISTS silver.crm_sales_details (
     sls_ord_num varchar(50),
     sls_prd_key varchar(50),
     sls_cust_id int,
-    sls_order_dt int,
-    sls_ship_dt int,
-    sls_due_dt int,
+    sls_order_dt date,
+    sls_ship_dt date,
+    sls_due_dt date,
     sls_sales int,
     sls_quantity int,
     sls_price int,
+    dwh_create_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS silver.crm_prd_info (
+    prd_id int,
+    prd_key varchar(50),
+    cat_id varchar(50),
+    prd_nm varchar(50),
+    prd_cost int,
+    prd_line varchar(50),
+    prd_start_dt Date,
+    prd_end_dt Date,
     dwh_create_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
